4fb2c79135e31290b15cd85c35c1455dfbddaba6
Harikirannaik <126312415+Harikirannaik@users.noreply.github.com>
2024-01-07 10:30:58 +0530

    upload

---
+++ ReturnHTMLadRes/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req,res)=>{
+    const data = fs.readFileSync('index.html').toString()
+    res.end(data);
+});
+server.listen(4436, ()=>{
+    console.log('server at 4436');
+});
