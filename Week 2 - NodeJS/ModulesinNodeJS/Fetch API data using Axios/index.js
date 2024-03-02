0f0ace00142e7c2caf8d043ec2b12fda1a72bdda
Harikirannaik <harikiran1252@gmail.com>
2024-03-02 10:36:58 +0530

    new

---
+++ Week 2 - NodeJS/ModulesinNodeJS/Fetch API data using Axios/index.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+const Solution = async () => {
+  // Write your code here
+  try {
+    const response = await axios.get(
+      `https://api.codingninjas.com/api/v3/event_tags`)
+
+    const responseData = response.data;
+    console.log(responseData);
+  
+  } catch (error) {
+    console.log("Error in fetching", error.message);
+  }
+
+};
+Solution();
+module.exports = Solution;
